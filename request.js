a2da682da5b2a98c3b86611ed1fce3d1019430d3
Kevin Locke <kevin@kevinlocke.name>
2014-07-03 15:12:10 -0600

    Support gzip with encoding on node pre-v0.9.4
    setEncoding was added to zlib streams in joyent/node@9b5abe5 (v0.9.4 and
    later).  In order to support encoding and gzip together on earlier
    releases, pass the data through an additional Stream which decodes the
    data using the stringstream module.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- request.js
+++ request.js
@@ -18,6 +18,7 @@ var optional = require('./lib/optional')
   , mime = require('mime-types')
   , tunnel = optional('tunnel-agent')
   , _safeStringify = require('json-stringify-safe')
+  , stringstream = optional('stringstream')
 
   , ForeverAgent = require('forever-agent')
   , FormData = optional('form-data')
@@ -948,8 +949,13 @@ Request.prototype.onResponse = function (response) {
     if (self.encoding) {
       if (self.dests.length !== 0) {
         console.error("Ignoring encoding parameter as this stream is being piped to another stream which makes the encoding option invalid.")
-      } else {
+      } else if (dataStream.setEncoding) {
         dataStream.setEncoding(self.encoding)
+      } else {
+        // Should only occur on node pre-v0.9.4 (joyent/node@9b5abe5) with
+        // zlib streams.
+        // If/When support for 0.9.4 is dropped, this should be unnecessary.
+        dataStream = dataStream.pipe(stringstream(self.encoding))
       }
     }
 
